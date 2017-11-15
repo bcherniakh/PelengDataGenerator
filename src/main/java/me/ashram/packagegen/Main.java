6348ca5fd6ce1d3c31599e1506097070bf0b8bba
Bohdan Cherniakh <ashram.astartes@gmail.com>
2017-11-15 22:06:44 +0200

    add Gradle build sytem to the project

--- src/main/java/me/ashram/packagegen/Main.java
+++ src/main/java/me/ashram/packagegen/Main.java
@@ -8,16 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/package_generator.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("view/package_generator.fxml"));
         primaryStage.setTitle("Package generator");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class DummyTest {
+
+    @Test
+    public void testThisApplicationPlease() throws Exception {
+        fail("No tests implemented yet");
+    }
+}
